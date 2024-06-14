df0774461150dfbd430e41142f0125f478dfeb43
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-14 14:31:53 +0600

    leetcode day -2

---
+++ 01_array_hashing/10_pascals_triangle.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+        
+        
+https://leetcode.com/problems/pascals-triangle/description/
+        
+*/         
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        for (int i = 0; i < numRows; i++) {
+            vector<int> row(i + 1, 1);
+            for (int j = 1; j < i; j++) {
+                row[j] = result[i - 1][j - 1] + result[i - 1][j];
+            }
+            result.push_back(row);
+        }
+        return result;
+    }
+}; 
+int main(){
+    
+    
+    return 0;
+}
