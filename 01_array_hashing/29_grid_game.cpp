fa17e56f2d63427d4c2f1b9d102d303c8d15ff0c
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-21 01:05:14 +0600

    leetcode day - 9

---
+++ 01_array_hashing/29_grid_game.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+        
+        
+https://leetcode.com/problems/grid-game/
+        
+*/  
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        long long topSum = accumulate(begin(grid[0]), end(grid[0]), 0LL), bottomSum = 0;
+        long long ans = LLONG_MAX;
+        for (int i = 0; i < grid[0].size(); i++) {
+            topSum -= grid[0][i];
+            ans = min(ans, max(topSum, bottomSum));
+            bottomSum += grid[1][i];
+        }
+        return ans;
+    }
+};        
+int main(){
+    
+    
+    return 0;
+}
