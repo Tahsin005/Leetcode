6973897f5d77a3670db9dbc1be0a6db423484a10
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-23 22:52:17 +0600

    leetcode day - 12

---
+++ 01_array_hashing/34_design_hashset.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+        
+        
+https://leetcode.com/problems/design-hashset/description/
+        
+*/  
+class MyHashSet {
+public:
+    vector<bool> mp;
+    MyHashSet() {
+        mp.resize(1000001, false);
+    }
+    
+    void add(int key) {
+        mp[key] = true;
+    }
+    
+    void remove(int key) {
+        mp[key] = false;
+    }
+    
+    bool contains(int key) {
+        return mp[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */        
+int main(){
+    
+    
+    return 0;
+}
