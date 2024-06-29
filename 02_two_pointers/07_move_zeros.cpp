f6a863acf4af4b69726b763d26778fdf8b09d8f0
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-29 14:27:24 +0600

    leetcode day - 19

--- 02_two_pointers/07_move_zeros.cpp
+++ 02_two_pointers/07_move_zeros.cpp
@@ -23,7 +23,22 @@ public:
         }
 
     }
-};        
+};       
+
+// Anothe solution : 
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] != 0) {
+                swap(nums[right], nums[left]);
+                left++;
+            }
+        }        
+    }
+};
 int main(){
     
     
