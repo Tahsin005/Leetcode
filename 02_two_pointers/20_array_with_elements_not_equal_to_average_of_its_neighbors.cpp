72dc555e049fbf70861f8842435c51faa9f47d72
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-09 15:16:59 +0600

    leetcode day - 28

---
+++ 02_two_pointers/20_array_with_elements_not_equal_to_average_of_its_neighbors.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+        
+        
+https://leetcode.com/problems/array-with-elements-not-equal-to-average-of-neighbors/
+        
+*/  
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int i = 0, j = nums.size() - 1;
+
+        vector<int> ans;
+        while (i < j) {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+            i++, j--;
+        }
+        if (nums.size() & 1) ans.push_back(nums[i]);
+
+        return ans;
+    }
+};        
+int main(){
+    
+    
+    return 0;
+}
