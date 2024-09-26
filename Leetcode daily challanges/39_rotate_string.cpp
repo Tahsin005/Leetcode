6f60f06b1fc4abe9d6829b4c8e8af2424f6a5cb5
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-26 09:51:59 +0600

    day - 46

---
+++ Leetcode daily challanges/39_rotate_string.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+        
+        
+https://leetcode.com/problems/rotate-string/
+        
+*/          
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        
+        string concatenated = s + s;
+
+        if (concatenated.find(goal) != string::npos) {
+            return true;
+        }
+
+        return false;
+    }
+};
+int main(){
+    
+    
+    return 0;
+}
