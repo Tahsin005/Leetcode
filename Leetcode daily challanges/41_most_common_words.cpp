57b83609bbcf07f30b113860a2baf9c9699915b8
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-28 14:17:12 +0600

    day - 48

---
+++ Leetcode daily challanges/41_most_common_words.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+        
+        
+https://leetcode.com/problems/most-common-word/
+        
+*/          
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        for (auto &ch: paragraph) {
+            if(!isalpha(ch)) ch = ' ';
+            ch = tolower(ch);
+        }
+
+        map<string,int> mp, band;
+        for (auto s: banned) band[s]++;
+
+        string s, ans;
+        stringstream ss(paragraph);
+        while(ss>>s)
+            if (band[s] == 0) mp[s]++;
+
+        int mx = INT_MIN;
+        for (auto &[a, b]: mp)
+            if(b > mx) ans = a, mx = b;
+        return ans;
+    }
+};
+int main(){
+    
+    
+    return 0;
+}
