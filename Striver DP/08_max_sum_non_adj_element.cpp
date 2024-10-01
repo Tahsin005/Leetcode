69d2db0944f5f9985dc74d7a8fdcf078483b7b33
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-10-01 08:56:21 +0600

    dp - striver - max sum of non adjacent elements in an arrayList - tabulation - space optimization form o(n) to o(1) - day - 51

--- Striver DP/08_max_sum_non_adj_element.cpp
+++ Striver DP/08_max_sum_non_adj_element.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h> 
 using namespace std;
+
 int solution(int idx, vector<int> &arr, vector<int> &dp) {
     if (dp[idx] != -1) {
         return dp[idx];
@@ -22,22 +23,36 @@ int maximumNonAdjacentSum(vector<int> &nums){
     int n = nums.size();
 
     // return solve(n, nums);
-    vector<int> dp(n, 0);
+    // vector<int> dp(n, 0);
 
-    dp[0] = nums[0];
-    int neg = 0;
+    // dp[0] = nums[0];
+    // int neg = 0;
+
+    // for (int i = 1; i < n; i++) {
+    //     int take = nums[i];
+    //     if (i > 1) take += dp[i - 2];
+    //     int notTake = 0 + dp[i - 1];
+
+    //     dp[i] = max(take, notTake);
+    // }
+
+    // return dp[n - 1];
+
+    int prev = nums[0];
+    int prev2 = 0;
 
     for (int i = 1; i < n; i++) {
         int take = nums[i];
-        if (i > 1) take += dp[i - 2];
-        int notTake = 0 + dp[i - 1];
+        if (i > 1) take += prev2;
+        int notTake = 0 + prev;
 
-        dp[i] = max(take, notTake);
+        int curr = max(take, notTake);
+        prev2 = prev;
+        prev = curr;
     }
 
-    return dp[n - 1];
+    return prev;
 }
-
 int main(){
-    
+
 }
