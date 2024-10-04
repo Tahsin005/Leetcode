e677d5f1abc1f5b7fbf6d38720e79da7a7088ab2
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-10-04 09:31:42 +0600

    day - 54

---
+++ Leetcode daily challanges/47_divisor_game.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+        
+        
+https://leetcode.com/problems/divisor-game/
+        
+*/  
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if (n & 1) return false;
+        return true;
+    }
+};        
+int main(){
+    
+    
+    return 0;
+}
