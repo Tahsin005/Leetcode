8d9fa0d2ac7185feb4ddca1a37c1074245910019
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-10-05 15:55:21 +0600

    day - 55

---
+++ Striver DP/11_nth_tribonacci.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+        
+        
+https://leetcode.com/problems/n-th-tribonacci-number/
+        
+*/          
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int t0 = 0, t1 = 1, t2 = 1, tn;
+
+        for (int i = 3; i <= n; i++) {
+            tn = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2; 
+            t2 = tn; 
+        }
+
+        return tn;
+    }
+};
+int main(){
+    
+    
+    return 0;
+}
