7e8a3265431a4e407422f13fe1a9b27a56b0ae10
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-10-07 10:35:13 +0600

    day - 57

---
+++ Leetcode daily challanges/48_atn.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+        
+        
+https://leetcode.com/problems/add-two-integers/description/
+        
+*/  
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};        
+int main(){
+    
+    
+    return 0;
+}
