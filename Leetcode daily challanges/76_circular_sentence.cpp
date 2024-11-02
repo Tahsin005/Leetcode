f470bf24a605471d9d8714624dda70444f34ee6a
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-11-02 15:56:01 +0600

    day - 83

---
+++ Leetcode daily challanges/76_circular_sentence.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+
+
+https://leetcode.com/problems/circular-sentence/?envType=daily-question&envId=2024-11-02
+
+*/
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        stringstream ss(sentence);
+        string word;
+
+        vector<string> words;
+
+        while (ss >> word) {
+            words.push_back(word);
+        }
+        int n = words.size();
+
+        char last = words[n - 1].back();
+
+        for (int i = 0; i < n; i++) {
+            if (words[i].front() != last) return false;
+            last = words[i].back();
+        }
+
+        return true;
+    }
+};
+int main(){
+
+
+    return 0;
+}
