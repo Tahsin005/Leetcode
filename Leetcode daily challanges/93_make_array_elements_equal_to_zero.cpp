2420b55e7a9aac80b147162df6d51b2ced36e229
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-11-17 08:39:14 +0600

    day - 98

--- Leetcode daily challanges/93_make_array_elements_equal_to_zero.cpp
+++ Leetcode daily challanges/93_make_array_elements_equal_to_zero.cpp
@@ -7,6 +7,39 @@ using namespace std;
 https://leetcode.com/contest/weekly-contest-424/problems/make-array-elements-equal-to-zero/
 
 */
+class Solution {
+public:
+    int countValidSelections(vector<int>& nums) {
+        int n = nums.size();
+        int validSelections = 0;
+
+        auto simulate = [&](int start, int direction) -> bool {
+            vector<int> temp = nums;
+            int curr = start;
+
+            while (curr >= 0 && curr < n) {
+                if (temp[curr] == 0) {
+                    curr += direction;
+                } else {
+                    temp[curr]--;
+                    direction = -direction;
+                    curr += direction;
+                }
+            }
+
+            return all_of(temp.begin(), temp.end(), [](int x) { return x == 0; });
+        };
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                if (simulate(i, -1)) validSelections++;
+                if (simulate(i, 1)) validSelections++;
+            }
+        }
+
+        return validSelections;
+    }
+};
 int main(){
 
 
