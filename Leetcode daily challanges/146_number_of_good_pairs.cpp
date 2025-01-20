9c3fd8648ebfadf324b3247fee73808c72c4b3f8
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-01-20 13:37:33 +0600

    day - 162

---
+++ Leetcode daily challanges/146_number_of_good_pairs.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+
+https://leetcode.com/problems/number-of-good-pairs/description/
+
+*/
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int, int> pairs;
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (pairs.find(nums[i]) != pairs.end()) {
+                count += pairs[nums[i]];
+            }
+            pairs[nums[i]] = pairs[nums[i]] + 1;
+        }
+
+        return count;        
+    }
+};
+int main() {
+
+
+    return 0;
+}
