ca7c5e8290fa723f93d20e43e2afa886c7437df0
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-19 20:28:31 +0600

    day - 192

---
+++ Leetcode daily challanges/185_mean_of_array_after_removing_some_element.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+
+https://leetcode.com/problems/defanging-an-ip-address/description/
+
+*/
+class Solution {
+    public:
+        double trimMean(vector<int>& arr) {
+            int n = arr.size();
+            int a = (int)(5.0 / 100 * n);
+
+            sort(arr.begin(), arr.end());
+
+            double sum = 0;
+            int cnt = 0;
+            for (int i = a; i < n - a; i++) {
+                sum += arr[i];
+                cnt++;
+            }
+
+            return sum / cnt;
+        }
+};
+
+int main() {
+
+
+    return 0;
+}
