f4c0f60c4f0aabf911a800b845aa8911d5829c36
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-21 12:12:14 +0600

    day - 194

---
+++ Leetcode daily challanges/187_find_special_substring_of_length_k.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+
+https://leetcode.com/problems/find-special-substring-of-length-k/description/
+
+*/
+class Solution {
+    public:
+        bool hasSpecialSubstring(string s, int k) {
+            int cnt = 1;
+
+            for (int i = 1; i < s.length(); i++, cnt++) {
+                if (s[i] != s[i - 1] and cnt == k) return true;
+                if (s[i] != s[i - 1]) cnt = 0;
+            }
+
+            return cnt == k;
+        }
+};
+int main() {
+
+
+    return 0;
+}
