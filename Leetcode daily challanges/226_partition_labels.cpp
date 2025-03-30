aece06f31b83a5ad1b0f1f4d2cd9d9b3f08e47dd
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-03-30 13:53:35 +0600

    day - 231

---
+++ Leetcode daily challanges/226_partition_labels.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+
+https://leetcode.com/problems/partition-labels/description/?envType=daily-question&envId=2025-03-30
+
+*/
+class Solution {
+    public:
+        vector<int> partitionLabels(string s) {
+            vector<int> lastOccurrence(26, 0);
+            for (int i = 0; i < s.size(); ++i) {
+                lastOccurrence[s[i] - 'a'] = i;
+            }
+    
+            int partitionEnd = 0, partitionStart = 0;
+            vector<int> partitionSizes;
+    
+            for (int i = 0; i < s.size(); ++i) {
+                partitionEnd = max(partitionEnd, lastOccurrence[s[i] - 'a']);
+    
+                if (i == partitionEnd) {
+                    partitionSizes.push_back(i - partitionStart + 1);
+                    partitionStart = i + 1;
+                }
+            }
+            
+            return partitionSizes;
+        }
+};
+int main() {
+
+
+    return 0;
+}
