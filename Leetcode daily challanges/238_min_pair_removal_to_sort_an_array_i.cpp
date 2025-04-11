b26bbbcfad46ddfb3d9a70c640dcaf19b21ee336
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-04-11 12:37:05 +0600

    day - 243

---
+++ Leetcode daily challanges/238_min_pair_removal_to_sort_an_array_i.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+/*
+
+https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i/description/
+
+*/
+class Solution {
+    public:
+        bool isNondecreasing(vector<int>& nums) {
+            for (int i = 1; i < nums.size(); i++) {
+                if (nums[i] < nums[i - 1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        int minimumPairRemoval(vector<int>& nums) {
+            int ops = 0;
+            while (!isNondecreasing(nums)) {
+                int min_sum = INT_MAX;
+                int index = -1;
+
+                for (int i = 0; i < nums.size() - 1; i++) {
+                    int sum = nums[i] + nums[i + 1];
+                    if (sum < min_sum) {
+                        min_sum = sum;
+                        index = i;
+                    }
+                }
+
+                vector<int> temp;
+                for (int i = 0; i < nums.size(); i++) {
+                    if (i == index) {
+                        temp.push_back(nums[i] + nums[i + 1]);
+                        i++;
+                    } else {
+                        temp.push_back(nums[i]);
+                    }
+                }
+                nums = temp;
+                ops++;
+            }
+            return ops;
+        }
+};
+int main() {
+
+
+    return 0;
+}
