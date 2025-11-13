efbea3323929ef13816519be527781a353392ec7
MD. Tahsin Ferdous <124136023+Tahsin005@users.noreply.github.com>
2025-11-13 11:05:16 +0600

    day-2

---
+++ leetcode2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+class Solution {
+public:
+    int maxOperations(string s) {
+        int countOne = 0;
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '0') {
+                while ((i + 1) < s.length() && s[i + 1] == '0') {
+                    i++;
+                }
+                ans += countOne;
+            } else {
+                countOne++;
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+}
