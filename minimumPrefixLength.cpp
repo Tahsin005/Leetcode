2f74d069d9e95fe43d6ff4a7a192d6b806f7cf23
MD. Tahsin Ferdous <124136023+Tahsin005@users.noreply.github.com>
2026-01-25 16:00:17 +0600

    Create minimumPrefixLength.cpp

---
+++ minimumPrefixLength.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minimumPrefixLength(vector<int>& nums) {
+        int i = nums.size() - 1;
+        while (i > 0 and nums[i - 1] < nums[i]) {
+            i--;
+        }
+        return i;
+    }
+};
