667cbca3db488dc1ed45111cf439771153e15d7d
MD. Tahsin Ferdous <124136023+Tahsin005@users.noreply.github.com>
2026-02-09 14:37:22 +0600

    Create findSubstring.go

---
+++ findSubstring.go
@@ -0,0 +1,39 @@
+func findSubstring(s string, words []string) []int {
+    wordFrequency := make(map[string]int)
+    
+    for _, word := range words {
+        wordFrequency[word]++
+    }
+    
+    var res []int
+    
+    length := len(words[0])
+    
+    for i := 0; i < len(s)-length*len(words)+1; i++ {
+        seen := make(map[string]int)
+        
+        for j := 0; j < len(words); j++ {
+            nextIndex := i + j*length
+            word := s[nextIndex: nextIndex + length]
+
+            if _, ok := wordFrequency[word]; !ok {
+                break
+            }
+            
+            seen[word]++
+            
+            seenFrequency, _ := seen[word]
+            originFrequency, _ := wordFrequency[word]
+            
+            if seenFrequency > originFrequency {
+                break
+            }
+ 
+            if j + 1 == len(words) {
+                res = append(res, i)
+            }
+        }
+    }
+    
+    return res
+}
